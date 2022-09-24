758d929606e4e2ebacb83bc0e96be1e0f7bcbd2e
Hiago <hiagotnt1@gmail.com>
2022-09-24 07:41:30 +0100

    remove unnecessary procedure-history controllers, services and dtos

--- src/modules/procedure-history/dto/update-procedure-history.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateProcedureHistoryDto } from './create-procedure-history.dto';
-
-export class UpdateProcedureHistoryDto extends PartialType(CreateProcedureHistoryDto) {}
--- src/modules/procedure-history/procedure-history.controller.ts
+++ src/modules/procedure-history/procedure-history.controller.ts
@@ -1,19 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
 import { ProcedureHistoryService } from './procedure-history.service'
-import { CreateProcedureHistoryDto } from './dto/create-procedure-history.dto'
-import { UpdateProcedureHistoryDto } from './dto/update-procedure-history.dto'
 import { permissions } from '../../config/permissions'
 import { RequiredPermission } from '../../decorators/permission.decorator'
 
 @Controller('procedure-history')
 export class ProcedureHistoryController {
   constructor(private readonly service: ProcedureHistoryService) {}
-
-  @RequiredPermission(permissions.procedureHistory.Create)
-  @Post()
-  create(@Body() dto: CreateProcedureHistoryDto) {
-    return this.service.create(dto)
-  }
 
   @RequiredPermission(permissions.procedureHistory.Read)
   @Get()
@@ -25,17 +17,5 @@ export class ProcedureHistoryController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.service.findOne(+id)
-  }
-
-  @RequiredPermission(permissions.procedureHistory.Update)
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() dto: UpdateProcedureHistoryDto) {
-    return this.service.update(+id, dto)
-  }
-
-  @RequiredPermission(permissions.procedureHistory.Delete)
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.service.remove(+id)
   }
 }
