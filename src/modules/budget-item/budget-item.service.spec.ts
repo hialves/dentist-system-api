8a7006ea764e245fc5fa22b4f3a2b22be7070385
Hiago <hiagotnt1@gmail.com>
2022-09-24 21:55:41 +0100

    budget tests

--- src/modules/budget-item/budget-item.service.spec.ts
+++ src/modules/budget-item/budget-item.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { BudgetItemRepositoryMock } from '../../test/utils/entity.mock'
 import { BudgetItemService } from './budget-item.service'
 
 describe('BudgetItemService', () => {
@@ -6,7 +7,7 @@ describe('BudgetItemService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BudgetItemService],
+      providers: [BudgetItemService, BudgetItemRepositoryMock],
     }).compile()
 
     service = module.get<BudgetItemService>(BudgetItemService)
--- src/modules/budget/budget.service.spec.ts
+++ src/modules/budget/budget.service.spec.ts
@@ -1,12 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { getRepositoryToken } from '@nestjs/typeorm'
-import { DataSource, Repository } from 'typeorm'
 import { BudgetService } from './budget.service'
-import { Budget } from './entities/budget.entity'
-import { createMock } from '@golevelup/ts-jest'
 import { BudgetItemService } from '../budget-item/budget-item.service'
-import { BudgetItem } from '../budget-item/entities/budget-item.entity'
-import { DataSourceMock } from '../../../test/utils/data-source.mock'
+import { DataSourceMock } from '../../test/utils/data-source.mock'
+import { BadRequestException } from '@nestjs/common'
+import { BudgetItemRepositoryMock, BudgetRepositoryMock } from '../../test/utils/entity.mock'
 
 describe('BudgetService', () => {
   let service: BudgetService
@@ -14,40 +11,95 @@ describe('BudgetService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [],
-      providers: [
-        BudgetService,
-        BudgetItemService,
-        {
-          provide: DataSource,
-          useValue: DataSourceMock,
-        },
-        {
-          provide: getRepositoryToken(Budget),
-          useValue: createMock<Repository<Budget>>(),
-        },
-        {
-          provide: getRepositoryToken(BudgetItem),
-          useValue: createMock<Repository<BudgetItem>>(),
-        },
-      ],
-      exports: [BudgetService],
+      providers: [BudgetService, BudgetItemService, DataSourceMock, BudgetRepositoryMock, BudgetItemRepositoryMock],
     }).compile()
 
     service = module.get<BudgetService>(BudgetService)
   })
 
   it('should be defined', () => {
-    const dto = {
-      clientId: 1,
-      clinicId: 1,
-      employeeId: 1,
-      procedureIds: [1, 2],
-    }
-    const entity = BudgetService.createEntity(dto)
+    expect(service).toBeDefined()
+  })
 
-    expect(entity.clientId).toBeDefined()
-    expect(entity.clinicId).toBeDefined()
-    expect(entity.employeeId).toBeDefined()
-    expect(entity.budgetItems).toBe(undefined)
+  describe('create', () => {
+    it('should have id, createdAt, updatedAt on return', async () => {
+      const dto = {
+        clientId: 1,
+        clinicId: 1,
+        employeeId: 1,
+        procedureIds: [1, 2],
+      }
+      const budget = BudgetService.createEntity(dto)
+      const result = await service.create(budget, dto.procedureIds)
+
+      expect(result.id).toBeDefined()
+      expect(result.budgetItems).toBeDefined()
+      expect(result.budgetItems.length).toBeGreaterThanOrEqual(1)
+    })
+  })
+
+  describe('createEntity', () => {
+    it('should create entity with all fks set', () => {
+      const dto = {
+        clientId: 1,
+        clinicId: 1,
+        employeeId: 1,
+        procedureIds: [1, 2],
+      }
+      const entity = BudgetService.createEntity(dto)
+
+      expect(entity.clientId).toBeDefined()
+      expect(entity.clinicId).toBeDefined()
+      expect(entity.employeeId).toBeDefined()
+      expect(entity.budgetItems).toBe(undefined)
+    })
+
+    it('should throw BadRequestException if one of fks is unset', () => {
+      const dto = {
+        clientId: 1,
+        clinicId: 1,
+        employeeId: 1,
+        procedureIds: [],
+      }
+
+      try {
+        BudgetService.createEntity({ ...dto, clientId: undefined })
+        expect(true).toBeFalsy()
+      } catch (e) {
+        expect(e).toBeInstanceOf(BadRequestException)
+        expect(e.status).toBe(400)
+      }
+
+      try {
+        BudgetService.createEntity({ ...dto, clinicId: undefined })
+        expect(true).toBeFalsy()
+      } catch (e) {
+        expect(e).toBeInstanceOf(BadRequestException)
+        expect(e.status).toBe(400)
+      }
+
+      try {
+        BudgetService.createEntity({ ...dto, employeeId: undefined })
+        expect(true).toBeFalsy()
+      } catch (e) {
+        expect(e).toBeInstanceOf(BadRequestException)
+        expect(e.status).toBe(400)
+      }
+    })
+
+    it('should not contain budgetItems, id, createdAt, updatedAt in Budget entity', () => {
+      const dto = {
+        clientId: 1,
+        clinicId: 1,
+        employeeId: 1,
+        procedureIds: [1, 2],
+      }
+      const entity = BudgetService.createEntity(dto)
+
+      expect(entity.budgetItems).toBeUndefined()
+      expect(entity.id).toBeUndefined()
+      expect(entity.createdAt).toBeUndefined()
+      expect(entity.updatedAt).toBeUndefined()
+    })
   })
 })
