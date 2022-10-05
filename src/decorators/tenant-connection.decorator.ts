c597efaa04c4deafdc7fd855f4babcd8663dcd79
Hiago <hiagotnt1@gmail.com>
2022-10-05 05:55:42 +0100

    remove console log

--- src/decorators/tenant-connection.decorator.ts
+++ src/decorators/tenant-connection.decorator.ts
@@ -3,6 +3,5 @@ import { TenantService } from '../modules/public/tenant/tenant.service'
 
 export const TenantConnection = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest() as Express.Request
-  console.log({ xablau: request.user.tenantSchema })
   return TenantService.getTenantConnection(request.user.tenantSchema)
 })
