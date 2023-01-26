7695efe60a9a8e8d5187a643676fc08f0fce65c8
Hiago Alves <hiagotnt1@gmail.com>
2023-01-26 16:09:38 +0000

    fix compile errors in tenant service tests

--- src/modules/public/tenant/tenant.service.spec.ts
+++ src/modules/public/tenant/tenant.service.spec.ts
@@ -1,18 +1,36 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { TenantService } from './tenant.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { forwardRef } from '@nestjs/common'
+import { TenantService } from './tenant.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { RoleModule } from '../../tenanted/role/role.module'
+import { ClinicModule } from '../../tenanted/clinic/clinic.module'
+import { EmployeeModule } from '../../tenanted/employee/employee.module'
+import { EmployeeClinicModule } from '../../tenanted/employee_clinic/employee-clinic.module'
+import { Tenant } from './entities/tenant.entity'
+import { ConfigModule } from '@nestjs/config'
+import { getInMemoryDatabaseConfig } from '../../../test/utils/in-memory-db'
 
 describe('TenantService', () => {
-  let service: TenantService;
+  let service: TenantService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRoot(getInMemoryDatabaseConfig(false)),
+        TypeOrmModule.forFeature([Tenant]),
+        RoleModule,
+        ClinicModule,
+        forwardRef(() => EmployeeModule),
+        EmployeeClinicModule,
+      ],
       providers: [TenantService],
-    }).compile();
+    }).compile()
 
-    service = module.get<TenantService>(TenantService);
-  });
+    service = module.get<TenantService>(TenantService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
