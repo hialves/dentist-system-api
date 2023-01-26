1526c093591ba4388297b4ad5e827b44802fa807
Hiago Alves <hiagotnt1@gmail.com>
2023-01-26 16:10:15 +0000

    remove unnused import

--- src/modules/public/tenant/dto/create-tenant.dto.ts
+++ src/modules/public/tenant/dto/create-tenant.dto.ts
@@ -1,4 +1,3 @@
-import { Column } from 'typeorm'
 import { CreateFirstClinicDto } from '../../../tenanted/clinic/dto/create-first-clinic.dto'
 
 export class CreateTenantDto extends CreateFirstClinicDto {}
