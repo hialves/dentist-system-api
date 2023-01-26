23be0d2a7d03ed27a7f782c445cbb64bbabeaca4
Hiago Alves <hiagotnt1@gmail.com>
2023-01-26 16:16:38 +0000

    separate config from instance memory database

--- src/test/utils/in-memory-db.ts
+++ src/test/utils/in-memory-db.ts
@@ -1,8 +1,8 @@
 import { join } from 'path'
-import { DataSource } from 'typeorm'
+import { DataSource, DataSourceOptions } from 'typeorm'
 
-export const InMemoryTypeormTestingDatabase = async (isTenantModule: boolean = true) => {
-  const dataSource = new DataSource({
+export const getInMemoryDatabaseConfig = (isTenantModule: boolean = true): DataSourceOptions => {
+  const config: DataSourceOptions = {
     type: 'better-sqlite3',
     database: ':memory:',
     dropSchema: true,
@@ -10,7 +10,13 @@ export const InMemoryTypeormTestingDatabase = async (isTenantModule: boolean = t
       join(__dirname, '..', '..', `modules/${isTenantModule ? 'tenanted' : 'public'}/**/**/*.entity{.js,.ts}`),
     ],
     synchronize: true,
-  })
+  }
+
+  return config
+}
+
+export const InMemoryTypeormTestingDatabase = async (isTenantModule: boolean = true) => {
+  const dataSource = new DataSource(getInMemoryDatabaseConfig(isTenantModule))
 
   await dataSource.initialize()
 
