c841378d0bbad0574ae6daa3235a6a5e62dc203a
Hiago Alves <hiagotnt1@gmail.com>
2023-01-26 16:15:18 +0000

    add missing mail module to import

--- src/modules/tenanted/employee/employee.module.ts
+++ src/modules/tenanted/employee/employee.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { EmployeeService } from './employee.service'
 import { EmployeeController } from './employee.controller'
 import { TenantModule } from '../../public/tenant/tenant.module'
+import { MailModule } from '../../../mail/mail.module'
 
 @Module({
-  imports: [TenantModule],
+  imports: [TenantModule, MailModule],
   controllers: [EmployeeController],
   providers: [EmployeeService],
   exports: [EmployeeService],
