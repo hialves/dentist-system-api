df277e6780bd1912616250dd1f3df841023b3471
Hiago Alves <hiagotnt1@gmail.com>
2023-01-26 16:14:12 +0000

    add decorators to dto

--- src/modules/tenanted/clinic/dto/create-first-clinic.dto.ts
+++ src/modules/tenanted/clinic/dto/create-first-clinic.dto.ts
@@ -1,7 +1,11 @@
+import { IsNotEmptyObject } from 'class-validator'
 import { CreateEmployeeDto } from '../../employee/dto/create-employee.dto'
 import { CreateClinicDto } from './create.dto'
 
 export class CreateFirstClinicDto {
+  @IsNotEmptyObject()
   employee: CreateEmployeeDto
+
+  @IsNotEmptyObject()
   clinic: CreateClinicDto
 }
